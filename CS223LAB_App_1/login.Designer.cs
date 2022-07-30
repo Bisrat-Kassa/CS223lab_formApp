17bd5afbc4c17c635e1ce90803c2b81bb82d5bd9
Bisrat-Kassa <107492307+Bisrat-Kassa@users.noreply.github.com>
2022-07-29 19:32:16 -0700

    2nd commit
    multi radio button added
    login designer added
    with action

--- CS223LAB_App_1/login.Designer.cs
+++ CS223LAB_App_1/login.Designer.cs
@@ -71,7 +71,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(311, 290);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(120, 63);
             this.button1.TabIndex = 4;
             this.button1.Text = "login";
             this.button1.UseVisualStyleBackColor = true;
